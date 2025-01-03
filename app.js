dbc518b6d23bd43dc120905b607fd262a7251b77
Maroje Raguz <maroje.raguz100@gmail.com>
2025-01-03 09:40:41 +0100

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();  // Load environment variables from .env
+require('dotenv').config();
 
 const express = require('express');
 const fileUpload = require('express-fileupload');
